350f24166ac21da30b69fe529fa16faacdfc5688
rrrrpg <1411546844@qq.com>
2026-04-11 16:26:03 +0800

    打包xml信息

---
+++ Assets/GameMain/Scripts/Editor/GameFrameworkConfigs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework;
+using UnityGameFramework.Editor.ResourceTools;
+using System.IO;
+
+namespace AVGGame.Editor
+{
+    public static class GameFrameworkConfigs
+    {
+        [ResourceEditorConfigPath]
+        public static string ResourceEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceEditor.xml"));
+    }
+}
